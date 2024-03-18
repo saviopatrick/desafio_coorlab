f434a1df74ba9efc7e89444ab3a296ced2397995
saviopatrick <saviopatrick09@gmail.com>
2024-03-18 13:57:28 -0300

    chore: add colors and height

--- app/coorlab-frontend/tailwind.config.js
+++ app/coorlab-frontend/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      height:{
+        'height': '42.00rem',
+      },
+      colors:{
+        'primary-color': '#2B2F42', 
+        
+      }
+    },
   },
   variants: {
     extend: {},
